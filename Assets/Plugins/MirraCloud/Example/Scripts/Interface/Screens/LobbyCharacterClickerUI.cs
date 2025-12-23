cdce7bb99c4f9bb2d09bbc550a8418c1b7703ee1
Petr-Kozlov <petrKozlouv@gmail.com>
2025-12-24 01:41:56 +0900

    add webgl template

---
+++ Assets/Plugins/MirraCloud/Example/Scripts/Interface/Screens/LobbyCharacterClickerUI.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using DG.Tweening;
+using Plugins.MirraCloud.Example.Scripts.Test;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+namespace MirraCloud.Example
+{
+    public class LobbyCharacterClickerUI : MonoBehaviour, IPointerClickHandler
+    {
+        [SerializeField] private RectTransform _characterTransform;
+        
+        private LeaderboardTest _leaderboardTest;
+        private Sequence _sequence;
+
+        private void Awake()
+        {
+            _leaderboardTest = FindObjectOfType<LeaderboardTest>();
+        }
+
+        public void OnPointerClick(PointerEventData eventData)
+        {
+            _leaderboardTest.SendEvent();
+
+            _sequence?.Kill();
+            _characterTransform.transform.localScale = Vector3.one;
+
+            _sequence = DOTween.Sequence();
+            
+            _sequence
+                .Append(_characterTransform.DOScale(1.1f, 0.2f))
+                .Append(_characterTransform.DOScale(1f, 0.2f))
+                .SetEase(Ease.InOutSine);
+        }
+    }
+}
